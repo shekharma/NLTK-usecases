5fa5e0355ea3077126bb48f3cc2646949e620cbf
Shekhar Ashok Mohite <122733304+shekharma@users.noreply.github.com>
2024-04-07 10:27:31 +0530

    Create Tokenizer.py

---
+++ Tokenizer.py
@@ -0,0 +1,9 @@
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+text = "NLTK is a powerful Python library for natural language processing."
+words = word_tokenize(text)
+sentences = sent_tokenize(text)
+
+print(words)
+print(sentences)
